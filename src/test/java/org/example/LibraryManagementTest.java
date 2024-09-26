34e2b8144880048a12b1558ccfe8d68acda51b8f
Akshayraut9595 <akshayraut8080@gmail.com>
2024-09-27 00:26:24 +0530

    Created test for adding book in library

---
+++ src/test/java/org/example/LibraryManagementTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.print.Book;
+
+import static org.junit.jupiter.api.Assertions.*;
+class LibraryManagementTest {
+//    Test for adding book in library
+    @Test
+    void addBookTest(){
+        LibraryManagement library = new LibraryManagement();
+        Book book = new Book("A1","Object-Oriented Analysis and Design with Applications","Grady Booch",2007);
+        library.addBook(book);
+        assertTrue(library.isBookAvailable("A1"));
+    }
+}
