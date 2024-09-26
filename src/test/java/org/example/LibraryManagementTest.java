de53d731b37d812dcaf159c3ad70e983171a193d
Akshayraut9595 <akshayraut8080@gmail.com>
2024-09-27 02:05:41 +0530

    Implemented test for show available books

--- src/test/java/org/example/LibraryManagementTest.java
+++ src/test/java/org/example/LibraryManagementTest.java
@@ -1,6 +1,10 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,6 +33,7 @@ class LibraryManagementTest {
         assertEquals("Book is not available",result2);
     }
 
+    // Test for return book
     @Test
     void returnBookTest(){
         LibraryManagement library = new LibraryManagement();
@@ -44,5 +49,18 @@ class LibraryManagementTest {
 
         int number_of_copies_returning = 2;
         assertTrue(library.returnBook(book,number_of_copies_returning, user));
+    }
+
+    // Test for showing available books in library
+    @Test
+    void showAvailableBooksTest(){
+        LibraryManagement library = new LibraryManagement();
+        Book book1 = new Book("A1","Object-Oriented Analysis and Design with Applications","Grady Booch",2007, 2);
+        Book book2 = new Book("B1","Design Patterns: Elements of Reusable Object-Oriented Software"," Erich Gamma",1994, 2);
+        library.addBook(book1);
+        library.addBook(book2);
+
+        ArrayList<Book> availableBooks = new ArrayList<>(Arrays.asList(book1, book2));
+        assertEquals(availableBooks,library.showAvailableBooks());
     }
 }
