134a6080e939e61089217e076bca6ee059a45cc2
Akshayraut9595 <akshayraut8080@gmail.com>
2024-09-27 01:03:58 +0530

    Implemented test for borrowing book from library

--- src/test/java/org/example/LibraryManagementTest.java
+++ src/test/java/org/example/LibraryManagementTest.java
@@ -5,12 +5,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 class LibraryManagementTest {
-//    Test for adding book in library
+    //Test for adding book in library
     @Test
     void addBookTest(){
         LibraryManagement library = new LibraryManagement();
         Book book = new Book("A1","Object-Oriented Analysis and Design with Applications","Grady Booch",2007);
         library.addBook(book);
         assertTrue(library.isBookAvailable("A1"));
+    }
+
+    //Test for borrow book from library
+    @Test
+    void borrowBookTest(){
+        LibraryManagement library = new LibraryManagement();
+        Book book = new Book("A1","Object-Oriented Analysis and Design with Applications","Grady Booch",2007);
+        assertEquals("Book borrow successfully", library.borrowBook(book));
+        assertEquals("Book is not available", library.borrowBook(book));
     }
 }
